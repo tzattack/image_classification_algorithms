c7b685a3afb5df9578b69f012a17a56812c0a6f5
tzattack <tzattack@gmail.com>
2019-01-11 17:48:31 +0800

    initial update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# image_classification_algorithms
-A tensorflow implementation of classic deep learning image classification algorithms.
+## Feature
+
+### Main usage
+将tfrecord文件放入data_input目录并运行：
+>python3 predict.py
+
+输出结果分别存储在
+`./model_output/processed_files/test_"$model"_processed_files.json`
+`./model_output/processed_files/test_"$model"_classify_result.json`
+文件中。
+
+### Train new models
+将/datasets/parameters.txt文件中的内容改为分类的总数。
+
+将数据按分类放入`data_raw`文件夹下并将数据转为TFRecord格式：
+>python3 convert.py
+
+执行本命令进行训练：
+>python3 train.py
+
+导出计算图
+>python3 export_inference_graph.py
+
+模型名称为train文件夹下最新的模型
+>python3 freeze_graph.py --input_checkpoint=./train/"$model.ckpt-xxxx"
+
+将训练好的模型放入model文件夹下，并在predict.py中第114行修改model名称即可使用。
---
+++ datasets/dataset_factory.py
@@ -0,0 +1,29 @@
+"""A factory-pattern class which returns classification image/label pairs."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datasets import customized
+
+def get_dataset(name, split_name, dataset_dir, file_pattern=None, reader=None):
+  """Given a dataset name and a split_name returns a Dataset.
+
+  Args:
+    name: String, the name of the dataset.
+    dataset_dir: The directory where the dataset files are stored.
+    file_pattern: The file pattern to use for matching the dataset source files.
+    reader: The subclass of tf.ReaderBase. If left as `None`, then the default
+      reader defined by each dataset is used.
+
+  Returns:
+    A `Dataset` class.
+
+  Raises:
+    ValueError: If the dataset `name` is unknown.
+  """
+  return customized.get_split(
+      split_name,
+      dataset_dir,
+      file_pattern,
+      reader)
